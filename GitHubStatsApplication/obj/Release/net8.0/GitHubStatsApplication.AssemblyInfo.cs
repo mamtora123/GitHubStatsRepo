a8936ddfe13423bf6e37afd2dc25c59164468296
Priyanka <mamtora.12@gmail.com>
2024-04-24 16:11:26 +0100

    Changes related to version commited

--- GitHubStatsApplication/obj/Release/net8.0/GitHubStatsApplication.AssemblyInfo.cs
+++ GitHubStatsApplication/obj/Release/net8.0/GitHubStatsApplication.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("GitHubStatsApplication")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76eb42b8578a4b94dfbca2eb961f07240a2f2017")]
 [assembly: System.Reflection.AssemblyProductAttribute("GitHubStatsApplication")]
--- GitHubStatsTest/obj/Debug/net8.0/GitHubStatsTest.AssemblyInfo.cs
+++ GitHubStatsTest/obj/Debug/net8.0/GitHubStatsTest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GitHubStatsTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76eb42b8578a4b94dfbca2eb961f07240a2f2017")]
 [assembly: System.Reflection.AssemblyProductAttribute("GitHubStatsTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GitHubStatsTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
